b7eb33a51a55353fdf43413f4ede5ba9f63427d8
rowkaxl <rowkaxl@naver.com>
2019-05-05 13:24:04 +0900

    [update] 헤더추가

--- KakaobankTest/Scene/AppSearch/AppSearchModels.swift
+++ KakaobankTest/Scene/AppSearch/AppSearchModels.swift
@@ -11,7 +11,6 @@
 //
 
 import UIKit
-import RealmSwift
 import RxDataSources
 
 enum AppSearch {
--- KakaobankTest/Scene/Model/RecentHistoryRealmItem.swift
+++ KakaobankTest/Scene/Model/RecentHistoryRealmItem.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import RealmSwift
 
 /// Realm Object
 class RecentHistoryRealmItem: Object {
